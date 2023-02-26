4e2ec88daff68b9b6b817f70bf5d9cb232124c30
SupremePi <76017561+SupremePi@users.noreply.github.com>
2023-02-25 23:01:25 -0500

    Add files via upload

---
+++ sb_toolkit/roms/steam/Steam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+    if [ ! -d /home/pi/PieMarquee3 ]; then
+    echo "$(tput setaf 6)
+    The marquee script is on and this application cant run with it on please turn it off and try again.
+    $(tput sgr0)"
+    sleep 5
+    clear   
+    else
+    /opt/retropie/supplementary/runcommand/runcommand.sh 0 _PORT_ steamlink 
+fi
