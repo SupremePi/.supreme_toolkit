29f153bd1e0c43a071ef760afc5b3a5cb7d3343a
Supreme Retropie <76017561+SupremePi@users.noreply.github.com>
2023-03-22 12:07:19 -0400

    Update basicinfo.sh

--- sb_toolkit/retropiemenu/basicinfo.sh
+++ sb_toolkit/retropiemenu/basicinfo.sh
@@ -16,40 +16,39 @@ function getIPAddress() {
 }
 
 echo "$(tput setaf 6)
-                                                                                                                                                               
-                                                                .,,,,.,*               ,,,.,,,.                                                                
-                                                           ,,  ..(,#(/(/.. .,     ,. . (,%*%*/*.. .,.                                                          
-                                                            *%%%%%%%%%%%%%%#/ ,  ..%#%%%%%%%%%%%%%%.,                                                          
-                                                          . /%%%%%((%%%%%%%%%%.* *%%%%%%%%%#/#%%%%%.,                                                          
-                                                           ..%%%%%%%%#,(%%%%%%/  %%%%%%#,*%%%%%%%%( .                                                          
-                                                             ,%%%%%%%%%%/./%%%.  (%%%,.%%%%%%%%%%# ,                                                           
-                                                              .%%%%%%%%%%%%.        *%%%%%%%%%%%( *                                                            
-                                                              . ./%%%%%%%%(         .%%%%%%%%#* ,                                                              
-                                                                *.  *#(,.              ,/##.  ,                                                                
-                                                                ,                             .                                                                
-                                                              ,                                 ,                                                              
-                                                             ,                                   .                                                             
-                                                             .                                    ,                                                            
-                                                                           .........              *                                                            
-                                                           .            ...............             ,                                                          
-                                                         ,              ...............              .                                                         
-                                                        ,     ./##/,.. ................  ..,/##/.     .                                                        
-                                                        ,     .#    .(#*.............../#(.    #.                                                              
-                                                        *    ../#       ./#(*.....*(#/.       (*..    .                                                        
-                                                         .     ../%%(,.   (#*.....*#(    ,(#%/.                                                                
-                                                          *      ......,,,.. .,... ..,,,......      .                                                          
-                                                           ,    ........     ,/./*     ........                                                                
-                                                                            ,%#.##,                *                                                           
-                                                            *           ... .,,.,,. ...           .                                                            
-                                                             ,                                   .                                                             
-                                                              .                                 *                                                              
-                                                                ..            ...            ./                                                                
-                                                                    ,.                    *                                                                    
-                                                                        *             .                                                                        
-  
-                                                                        SUPREME ULTRA V2                                                                                                                             
+                                                                                                                                                             
+                         .,,,,.,*               ,,,.,,,.                                                                
+                    ,,  ..(,#(/(/.. .,     ,. . (,%*%*/*.. .,.                                                          
+                      *%%%%%%%%%%%%%%#/ ,  ..%#%%%%%%%%%%%%%%.,                                                          
+                     . /%%%%%((%%%%%%%%%%.* *%%%%%%%%%#/#%%%%%.,                                                          
+                     ..%%%%%%%%#,(%%%%%%/  %%%%%%#,*%%%%%%%%( .                                                          
+                        ,%%%%%%%%%%/./%%%.  (%%%,.%%%%%%%%%%# ,                                                           
+                          .%%%%%%%%%%%%.        *%%%%%%%%%%%( *                                                            
+                          . ./%%%%%%%%(         .%%%%%%%%#* ,                                                              
+                             *.  *#(,.              ,/##.  ,                                                                
+                             ,                             .                                                                
+                           ,                                 ,                                                              
+                         ,                                   .                                                    
+                         .                                    ,                                                            
+                                       .........              *                                                            
+                       .            ...............             ,                                                          
+                     ,              ...............              .                                                         
+                    ,     ./##/,.. ................  ..,/##/.     .                                                        
+                    ,     .#    .(#*.............../#(.    #.                                                              
+                    *    ../#       ./#(*.....*(#/.       (*..    .                                                        
+                    .     ../%%(,.   (#*.....*#(    ,(#%/.                                                                
+                      *      ......,,,.. .,... ..,,,......      .                                                          
+                       ,    ........     ,/./*     ........                                                                
+                                         ,%#.##,                *                                                           
+                         *           ... .,,.,,. ...           .                                                            
+                           ,                                   .                                                             
+                            .                                 *                                                              
+                              ..            ...            ./                                                                
+                                  ,.                    *                                                                    
+                                       *             .                                                                        
+ 
+                                       SUPREME ULTRA V2                                                                                                                             
 $(tput setaf 2)
-`date +"%A, %e %B %Y, %r"`
 
 `uname -srmo`
 `lsb_release -ds`
@@ -69,16 +68,109 @@ $(tput setaf 6)Memory................: `cat /proc/meminfo | grep MemFree | awk '
 Running Processes.....: `ps ax | wc -l | tr -d " "`
 IP Addresses..........: $(getIPAddress) and `curl -4 icanhazip.com 2>/dev/null | awk '{print $NF; exit}'`
 $(tput setaf 7)
-........WEATHER.......:
-$(tput setaf 5)	 City		Temp	Condition
-LONDON, UK Weather....: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EUR|UK|UK001|LONDON" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-ATHENS, GR Weather....: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EUR|GR|GR007|ATHINAI" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-CHICAGO, US Weather...: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=0&locCode=NAM|US|IL|CHICAGO" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-N.Y., US Weather......: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=0&locCode=NAM|US|NY|NEWYORK" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-L.A., US Weather......: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=0&locCode=NAM|US|CA|LOSANGELES" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-TOKYO, JP Weather.....: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=ASI|JP|JA041|TOKYO" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-MELBOURNE, AU Weather.: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=OCN|AU|VIC|MELBOURNE" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
-AUCKLAND, NZ Weather..: `curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=OCN|NZ|NZ000|AUCKLAND" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p'`
+........YOUR INFO.......:
 $(tput sgr0)"
-echo
+
+DateColumn=34 # Default is 27 for 80 character line, 34 for 92 character line
+TimeColumn=61 # Default is 49 for   "   "   "   "    61 "   "   "   "
+
+curl wttr.in/?0 --silent --max-time 3 > /tmp/now-weather
+
+readarray aWeather < /tmp/now-weather
+rm -f /tmp/now-weather
+
+if [[ "${aWeather[0]}" == "Weather report:"* ]] ; then
+    WeatherSuccess=true
+    echo "${aWeather[@]}"
+else
+    WeatherSuccess=false
+    echo "+============================+"
+    echo "| Weather unavailable now!!! |"
+    echo "| Check reason with command: |"
+    echo "|                            |"
+    echo "| curl wttr.in/?0            |" 
+    echo "|   --silent --max-time 3    |"
+    echo "+============================+"
+    echo " "
+fi
+echo " "
+
+#--------- DATE -------------------------------------------------------------
+
+tput sc
+
+i=0
+while [ $((++i)) -lt 9 ]; do tput cuu1; done
+
+if [[ "$WeatherSuccess" == true ]] ; then
+    Column=$((DateColumn - 9))
+    tput cuf $Column
+    # Blank out ", country" with x spaces
+    printf "          "
+else
+    tput cuf $DateColumn
+fi
+
+cal > /tmp/terminal1
+tr -cd '\11\12\15\40\60-\136\140-\176' < /tmp/terminal1  > /tmp/terminal
+
+CalLineCnt=1
+Today=$(date +"%e")
+
+printf "\033[32m"
+
+while IFS= read -r Cal; do
+    printf "%s" "$Cal"
+    if [[ $CalLineCnt -gt 2 ]] ; then
+        tput cub 22
+        for (( j=0 ; j <= 18 ; j += 3 )) ; do
+            Test=${Cal:$j:2}
+            if [[ "$Test" == "$Today" ]] ; then
+                printf "\033[7m" 
+                printf "%s" "$Today"
+                printf "\033[0m"
+                printf "\033[32m"
+                tput cuf 1
+            else
+                tput cuf 3
+            fi
+        done
+    fi
+
+    tput cud1
+    tput cuf $DateColumn
+    CalLineCnt=$((++CalLineCnt))
+done < /tmp/terminal
+
+printf "\033[00m"
+echo ""
+
+tput rc
+
+#-------- TIME --------------------------------------------------------------
+
+tput sc
+
+i=0
+while [ $((++i)) -lt 9 ]; do tput cuu1; done
+tput cuf $TimeColumn
+
+if hash toilet 2>/dev/null; then
+    echo " $(date +"%I:%M %P") " | \
+        toilet -f future --filter border > /tmp/terminal
+elif hash figlet 2>/dev/null; then
+    date +"%I:%M %P" | figlet > /tmp/terminal
+else
+    date +"%I:%M %P" > /tmp/terminal
+fi
+
+while IFS= read -r Time; do
+    printf "\033[01;36m"
+    printf "%s" "$Time"
+    tput cud1
+    tput cuf $TimeColumn
+done < /tmp/terminal
+
+tput rc
+
 read -n 1 -s -r -p "Press any key to continue"
