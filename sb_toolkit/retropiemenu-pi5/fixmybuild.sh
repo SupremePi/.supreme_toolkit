d9cb18ac9ff645a58cd7d494507dfdd8d4ae7432
Supreme Retropie <76017561+SupremePi@users.noreply.github.com>
2023-12-04 21:15:42 -0500

    update

--- sb_toolkit/retropiemenu-pi5/fixmybuild.sh
+++ sb_toolkit/retropiemenu-pi5/fixmybuild.sh
@@ -6,24 +6,35 @@
 #SUPREME LINKS AND TOOLKIT FIX START#
 #####################################
 
+if [ -d /home/pi/.supreme_toolkit/sb_toolkit/_scripts/ ]; then
 . /home/pi/.supreme_toolkit/sb_toolkit/_scripts/supreme-functions
 . /home/pi/.supreme_toolkit/sb_toolkit/_scripts/supreme-vars
+fi
 
 if [ ! -d /home/pi/.supreme_toolkit ]; then
-    echo "OH NO THE TOOLKIT IS MISSING!!! WE WILL TRY AND DOWNLOAD NEW ONE NOW"
-    sleep 3 
-    net_check
-    if [ $NETCHECK = 1 ]; then dialog  --sleep 1 --title "OFFLINE ERROR!!" --msgbox " Offline ... Tool Kit Fix not Availible Please Connect To Internet!" 0 0
-    else
-	cd /home/pi/
-    git clone https://github.com/SupremePi/.supreme_toolkit.git
-	clear
-	cd $HOME/.supreme_toolkit
-	chmod 755 ./
-	echo "Will Now Reboot To Save Changes"
-	sleep 3 
-	sudo reboot
-fi
+
+dialog  --sleep 1 --title "OH NO!!" --msgbox "OH NO THE TOOLKIT IS MISSING!!! WE WILL TRY AND DOWNLOAD THE LATEST VERSION NOW." 0 0
+
+INTERNET_STATUS="UNKNOWN"
+TIMESTAMP=`date +%s`
+
+    ping -c 1 -W 0.7 8.8.4.4 > /dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+        if [ "$INTERNET_STATUS" != "UP" ]; then
+		    cd /home/pi/
+            git clone https://github.com/SupremePi/.supreme_toolkit.git
+	        clear
+	        cd $HOME/.supreme_toolkit
+	        chmod 755 ./
+			INTERNET_STATUS="UP"
+			clear
+	        fi	
+            else
+			dialog  --sleep 1 --title "OFFLINE ERROR!!" --msgbox "YOUR OFFLINE ... Some Fixes Require The Toolkit To Work!!! But It Was Missing?... When Availible Please Connect To Internet And Downloaded The Lastest Toolkit!" 0 0
+            sleep 2
+            INTERNET_STATUS="DOWN"
+	        clear
+    fi
 fi	
 
 ###################################
@@ -62,7 +73,7 @@ function main_menu() {
             2>&1 > /dev/tty)
 
         case "$choice" in
-            1) fix_menu_layout_rb_pi5  ;; 
+            1) fix_menu_pi5  ;; 
 	        2) supreme_online  ;;
 	        3) supreme_off  ;;
             4) supreme_restart  ;;
@@ -73,9 +84,21 @@ function main_menu() {
 	clear
 }
 
-function supreme_online() { 
+function fix_menu_pi5() {
+    if [ ! -d /home/pi/.supreme_toolkit ]; then
+	dialog --infobox "OH NO THE TOOLKIT IS MISSING!" 3 33; sleep 2;
+    else 
+    fix_menu_layout_rb_pi5 
+fi	
+}
+
+function supreme_online() {
+    if [ ! -d /home/pi/.supreme_toolkit ]; then
+    dialog --infobox "OH NO THE TOOLKIT IS MISSING!" 3 33; sleep 2;
+    else 
     bash $sb_toolkit/_scripts/supreme-online-tool.sh
     clear
+fi	
 }
 
 function supreme_off() {
@@ -98,16 +121,6 @@ function supreme_restart() {
 
 function none() {
 	clear
-}
-
-function net_check() {
-#-----------NET CHECKER-----------#
-wget -q --spider http://google.com
-if [ $? -eq 0 ]; then
-  NETCHECK=0
-else
-  NETCHECK=1
-fi
 }
 
 main_menu
